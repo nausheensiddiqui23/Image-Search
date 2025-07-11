5c605de0be23ef49ae3629f30b284d31f185a0e2
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-07-11 23:51:33 +0530

    new js codes

---
+++ project.js
@@ -0,0 +1,6 @@
+ async function searchImages(){
+    inputData = searchInutEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+ }
